01d15d9c19cb7a923ce7106b645f613cc8180700
Nivethitha Soundar Rajan <52025677+Nivethitha167@users.noreply.github.com>
2020-08-08 19:13:12 +0530

    Add files via upload

---
+++ urlshort.py
@@ -0,0 +1,11 @@
+from urllib.request import urlopen
+from urllib.parse import urlencode
+import contextlib
+ 
+def make_url_short(url):
+    req_url = "http://tinyurl.com/api-create.php?" + urlencode({'url':url})
+    with contextlib.closing(urlopen(req_url)) as response:
+        return response.read().decode('utf-8')
+
+k = make_url_short("https://www.linkedin.com/in/nivethitha-soundarrajan-7a5866189/")
+print(k)
